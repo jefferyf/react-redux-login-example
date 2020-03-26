c8cd76ca07f49f4065a3589af901c56c895a75d5
Jeffery Miller <jeffery.f@gmail.com>
2020-03-26 09:37:54 -0500

    Initial Commit of create-react-app with redux

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>
+            <pre>{JSON.stringify(this.props)}</pre>
+            <button onClick={this.simpleAction}>Test Redux Action</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps) (App);
